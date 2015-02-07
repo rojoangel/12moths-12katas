6b812c30110e62bf4f507b2cee58a6154455035b
rojoangel <rojo.angel@gmail.com>
2015-02-07 23:35:19 +0100

    prime factors kata completed

--- 1-prime-factors-kata/src/PrimeFactors.php
+++ 1-prime-factors-kata/src/PrimeFactors.php
@@ -4,8 +4,20 @@ namespace Kata;
 
 class PrimeFactors {
 
+    /**
+     * @param $value
+     * @return array
+     */
     public static function generate($value)
     {
-        return array(1);
+        $primes = array();
+
+        for ($candidate = 2; $value > 1; $candidate = $candidate + 1) {
+            for( ; $value%$candidate == 0; $value = $value / $candidate) {
+                array_push($primes, $candidate);
+            }
+        }
+
+        return $primes;
     }
 }
--- 1-prime-factors-kata/tests/PrimeFactorsTest.php
+++ 1-prime-factors-kata/tests/PrimeFactorsTest.php
@@ -6,7 +6,38 @@ class PrimeFactorsTest extends \PHPUnit_Framework_TestCase {
 
     public function testOne()
     {
-        $this->assertEquals(array(1), PrimeFactors::generate(1));
+        $this->assertEquals(array(), PrimeFactors::generate(1));
+    }
+
+    public function testTwo()
+    {
+        $this->assertEquals(array(2), PrimeFactors::generate(2));
+    }
+
+
+    public function testFour()
+    {
+        $this->assertEquals(array(2, 2), PrimeFactors::generate(4));
+    }
+
+    public function testSix()
+    {
+        $this->assertEquals(array(2, 3), PrimeFactors::generate(6));
+    }
+
+    public function testEight()
+    {
+        $this->assertEquals(array(2, 2, 2), PrimeFactors::generate(8));
+    }
+
+    public function testNine()
+    {
+        $this->assertEquals(array(3, 3), PrimeFactors::generate(9));
+    }
+
+    public function testTwentyFive()
+    {
+        $this->assertEquals(array(5, 5), PrimeFactors::generate(25));
     }
 
 }
