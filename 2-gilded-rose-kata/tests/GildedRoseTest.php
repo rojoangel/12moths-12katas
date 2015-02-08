7e04da3c16e9f895fc46522edcb95117db6190a1
rojoangel <rojo.angel@gmail.com>
2015-02-08 22:09:52 +0100

    1st test

--- 2-gilded-rose-kata/tests/GildedRoseTest.php
+++ 2-gilded-rose-kata/tests/GildedRoseTest.php
@@ -5,5 +5,17 @@ namespace Kata;
 
 class GildedRoseTest extends \PHPUnit_Framework_TestCase {
 
+    public function testAtTheEndOfEachDayOurSystemLowersBothValuesForEveryItem()
+    {
+        $items = array(
+            new Item("+5 Dexterity Vest", 10, 20),
+        );
+
+        $gildedRose = new GildedRose($items);
+        $updatedItems = $gildedRose->updateQuality();
+
+        $this->assertEquals(9, $updatedItems[0]->getSellIn());
+        $this->assertEquals(19, $updatedItems[0]->getQuality());
+
+    }
 }
- 
