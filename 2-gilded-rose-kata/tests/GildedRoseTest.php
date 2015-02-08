423104844063edf07953c60c502b0dc3ebcb9a89
rojoangel <rojo.angel@gmail.com>
2015-02-08 22:12:45 +0100

    2nd test

--- 2-gilded-rose-kata/tests/GildedRoseTest.php
+++ 2-gilded-rose-kata/tests/GildedRoseTest.php
@@ -18,4 +18,18 @@ class GildedRoseTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(19, $updatedItems[0]->getQuality());
 
     }
+
+    public function testOnceTheSellByDateHasPassedQualityDegradesTwiceAsFast()
+    {
+        $items = array(
+            new Item("+5 Dexterity Vest", 0, 20),
+        );
+
+        $gildedRose = new GildedRose($items);
+        $updatedItems = $gildedRose->updateQuality();
+
+        $this->assertEquals(-1, $updatedItems[0]->getSellIn());
+        $this->assertEquals(18, $updatedItems[0]->getQuality());
+
+    }
 }
