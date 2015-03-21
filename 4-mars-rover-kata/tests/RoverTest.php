fca31cef124f0cdf5a0c5307b1ea8b9ecb5f3858
rojoangel <rojo.angel@gmail.com>
2015-03-21 21:18:24 +0100

    extracted wrapping tests to a new class

--- 4-mars-rover-kata/tests/RoverTest.php
+++ 4-mars-rover-kata/tests/RoverTest.php
@@ -9,7 +9,6 @@ use Kata\Direction\North;
 use Kata\Direction\South;
 use Kata\Direction\West;
 use Kata\Grid\InfiniteGrid;
-use Kata\Grid\RectangularGrid;
 
 class RoverTest extends \PHPUnit_Framework_TestCase
 {
@@ -146,86 +145,6 @@ class RoverTest extends \PHPUnit_Framework_TestCase
     {
         $grid = new InfiniteGrid();
         $rover = new Rover(new West(), new Position(0, 0), $grid);
-
-        $rover->moveBackward();
-
-        $this->assertEquals(new Position(1, 0), $rover->getPosition());
-    }
-
-    public function testNorthEdgeIsWrappedInARectangularGridWhenMovingForward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new North(), new Position(0, 1), $grid);
-
-        $rover->moveForward();
-
-        $this->assertEquals(new Position(0, 0), $rover->getPosition());
-    }
-
-    public function testNorthEdgeIsWrappedInARectangularGridWhenMovingBackward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new South(), new Position(0, 1), $grid);
-
-        $rover->moveBackward();
-
-        $this->assertEquals(new Position(0, 0), $rover->getPosition());
-    }
-
-    public function testSouthEdgeIsWrappedInARectangularGridWhenMovingForward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new South(), new Position(0, 0), $grid);
-
-        $rover->moveForward();
-
-        $this->assertEquals(new Position(0, 1), $rover->getPosition());
-    }
-
-    public function testSouthEdgeIsWrappedInARectangularGridWhenMovingBackward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new North(), new Position(0, 0), $grid);
-
-        $rover->moveBackward();
-
-        $this->assertEquals(new Position(0, 1), $rover->getPosition());
-    }
-
-    public function testEastEdgeIsWrappedInARectangularGridWhenMovingForward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new East(), new Position(1, 0), $grid);
-
-        $rover->moveForward();
-
-        $this->assertEquals(new Position(0, 0), $rover->getPosition());
-    }
-
-    public function testEastEdgeIsWrappedInARectangularGridWhenMovingBackward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new West(), new Position(1, 0), $grid);
-
-        $rover->moveBackward();
-
-        $this->assertEquals(new Position(0, 0), $rover->getPosition());
-    }
-
-    public function testWestEdgeIsWrappedInARectangularGridWhenMovingForward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new West(), new Position(0, 0), $grid);
-
-        $rover->moveForward();
-
-        $this->assertEquals(new Position(1, 0), $rover->getPosition());
-    }
-
-    public function testWestEdgeIsWrappedInARectangularGridWhenMovingBackward()
-    {
-        $grid = new RectangularGrid(2, 2);
-        $rover = new Rover(new East(), new Position(0, 0), $grid);
 
         $rover->moveBackward();
 
