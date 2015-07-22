681cd8fd9180bdf290f94e1c77aac900b5660094
rojoangel <rojo.angel@gmail.com>
2015-07-22 22:23:48 +0200

    added provider for single value test

--- 5-binary-search-tree/tests/Kata/BinarySearchTreeTest.php
+++ 5-binary-search-tree/tests/Kata/BinarySearchTreeTest.php
@@ -7,9 +7,25 @@ use PHPUnit_Framework_TestCase as TestCase;
 
 class BinarySearchTreeTest extends TestCase
 {
-    public function testSingleValue()
+    /**
+     * @param int $value
+     * @dataProvider singleValueProvider
+     */
+    public function testSingleValue($value)
     {
-        $tree = new BinarySearchTree(4);
-        $this->assertEquals(4, $tree->getValue());
+        $tree = new BinarySearchTree($value);
+        $this->assertEquals($value, $tree->getValue());
+    }
+
+    /**
+     * @return array
+     */
+    public function singleValueProvider()
+    {
+        return [
+            'positive' => [4],
+            'zero' => [0],
+            'negative' => [-1]
+        ];
     }
 }
