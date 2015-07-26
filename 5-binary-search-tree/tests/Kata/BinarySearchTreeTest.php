694bbe6aa4eda32169ce654c72afc625ef0dea87
rojoangel <rojo.angel@gmail.com>
2015-07-26 20:34:41 +0200

    add postOrder + test

--- 5-binary-search-tree/tests/Kata/BinarySearchTreeTest.php
+++ 5-binary-search-tree/tests/Kata/BinarySearchTreeTest.php
@@ -169,4 +169,66 @@ class BinarySearchTreeTest extends TestCase
                 [3, 2, 1, 4, 6, 5]]
         ];
     }
+
+    /**
+     * @param $values
+     * @param $expectedSortedValues
+     * @dataProvider providerForTestPostOrder
+     */
+    public function testPostOrder($values, $expectedSortedValues)
+    {
+        $tree = BinarySearchTree::from($values);
+        $this->assertEquals($expectedSortedValues, $tree->postOrder());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerForTestPostOrder()
+    {
+        return [
+            'left value #1' => [
+                [4, 2],
+                [2, 4]],
+            'left value #2' => [
+                [6, 5],
+                [5, 6]],
+            'left value #3' => [
+                [2, 0],
+                [0, 2]],
+            'left value #4' => [
+                [0, -1],
+                [-1, 0]],
+            'right value #1' => [
+                [2, 4],
+                [4, 2]],
+            'right value #2' => [
+                [5, 6],
+                [6, 5]],
+            'right value #3' => [
+                [0, 2],
+                [2, 0]],
+            'right value #4' => [
+                [-1, 0],
+                [0, -1]],
+            'multiple values #1' => [
+                [2, 1, 4],
+                [1, 4, 2]],
+            'multiple values #2' => [
+                [1, 2, 4],
+                [4, 2, 1]],
+            'multiple values #3' => [
+                [4, 2, 1],
+                [1, 2, 4]],
+            'multiple values #4' => [
+                [4, 4, 1, null, 1, 2, 2],
+                [2, 1, 4]],
+            'multiple values #5' => [
+                [6, 5, 4, 3, 2, 1],
+                [1, 2, 3, 4, 5, 6]],
+            'multiple values #6' => [
+                [3, 2, 4, 6, 5, 1],
+                [1, 2, 5, 6, 4, 3]]
+        ];
+    }
 }
