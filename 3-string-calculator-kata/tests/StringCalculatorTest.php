15586642181b77d3df7cd2113b11ffdbead25e9d
rojoangel <rojo.angel@gmail.com>
2015-07-28 22:33:38 +0200

    add support for unknown amount of numbers

--- 3-string-calculator-kata/tests/StringCalculatorTest.php
+++ 3-string-calculator-kata/tests/StringCalculatorTest.php
@@ -30,7 +30,8 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase {
             'one value = 2' => array('2', 2),
             'two values #1' => array('01',1),
             'two values #2' => array('12',3),
-            'two values #3' => array('34',7)
+            'two values #3' => array('34',7),
+            'unknown amount of numbers' => array('123456789', 1+2+3+4+5+6+7+8+9)
         );
     }
 }
