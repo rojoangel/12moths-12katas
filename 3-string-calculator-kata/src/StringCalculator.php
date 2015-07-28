b5cad7533df20052524bf40da109e0384a4ce604
rojoangel <rojo.angel@gmail.com>
2015-07-28 22:52:29 +0200

    added support for \n separator

--- 3-string-calculator-kata/src/StringCalculator.php
+++ 3-string-calculator-kata/src/StringCalculator.php
@@ -5,6 +5,8 @@ namespace Kata;
 
 class StringCalculator {
 
+    const TOKENS = ",\n";
+
     /**
      * @param string $numbers
      * @return int
@@ -12,10 +14,10 @@ class StringCalculator {
     public function add($numbers)
     {
         $result = 0;
-        $token = strtok($numbers, ',');
+        $token = strtok($numbers, self::TOKENS);
         while($token !== false) {
             $result += intval($token);
-            $token = strtok(',');
+            $token = strtok(self::TOKENS);
         }
         return $result;
     }
--- 3-string-calculator-kata/tests/StringCalculatorTest.php
+++ 3-string-calculator-kata/tests/StringCalculatorTest.php
@@ -25,13 +25,14 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase {
     public function addProvider()
     {
         return array(
-            'empty string' => array('', 0),
-            'one value = 1' => array('1', 1),
-            'one value = 2' => array('2', 2),
-            'two values #1' => array('0,1', 0+1),
-            'two values #2' => array('1,2', 1+2),
-            'two values #3' => array('3,4', 3+4),
-            'unknown amount of numbers' => array('1,2,3,4,5,6,7,8,9,', 1+2+3+4+5+6+7+8+9+0)
+            'empty string' => array("", 0),
+            'one value = 1' => array("1", 1),
+            'one value = 2' => array("2", 2),
+            'two values #1' => array("0,1", 0+1),
+            'two values #2' => array("1,2", 1+2),
+            'two values #3' => array("3,4", 3+4),
+            'unknown amount of numbers' => array("1,2,3,4,5,6,7,8,9,", 1+2+3+4+5+6+7+8+9+0),
+            '\n as separator' => array("1\n2\n3,4,5,6\n7,8\n9,", 1+2+3+4+5+6+7+8+9+0)
         );
     }
 }
