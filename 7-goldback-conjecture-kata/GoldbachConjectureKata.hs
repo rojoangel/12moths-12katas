3eef56f28672d20774d6ddbe64814f84cdd55580
Angel Rojo <rojoa@gsicommerce.com>
2015-08-17 22:36:22 +0200

    1st iteration as created during coding dojo

---
+++ 7-goldback-conjecture-kata/GoldbachConjectureKata.hs
@@ -0,0 +1,10 @@
+module GoldbachConjectureKata 
+(partition_of, is_prime) where
+
+is_prime 1 = False
+is_prime n = [] == [x | x<- [2..(n-1)], (mod n x) == 0]
+
+partition_of :: Int -> [(Int,Int)]
+partition_of n 
+	| odd n = []
+	| otherwise = [(x, n-x) | x <- [2.. (div n 2)], is_prime x && is_prime (n - x)]
