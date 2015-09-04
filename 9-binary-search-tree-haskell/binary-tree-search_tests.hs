052a9813487962ccd9c62e4b790d4d6c60779198
rojoangel <rojo.angel@gmail.com>
2015-09-04 21:31:13 +0200

    test from list

--- 9-binary-search-tree-haskell/binary-tree-search_tests.hs
+++ 9-binary-search-tree-haskell/binary-tree-search_tests.hs
@@ -26,5 +26,22 @@ binaySearchTreeTests =
     Node 4 (Node 3 EmptyTree EmptyTree) EmptyTree @=? treeInsert 3 (Node 4 EmptyTree EmptyTree)
     , testCase "add left value to a Node" $
     Node 4 EmptyTree (Node 5 EmptyTree EmptyTree) @=? treeInsert 5 (Node 4 EmptyTree EmptyTree)
-
+    , testCase "tree from list one elem" $
+    Node 1 EmptyTree EmptyTree @=? fromList [1]
+    , testCase "tree from list one two elements 1" $
+    Node 2 (Node 1 EmptyTree EmptyTree) EmptyTree @=? fromList[1,2]
+    , testCase "tree from list one two elements 2" $
+    Node 1 EmptyTree (Node 2 EmptyTree EmptyTree) @=? fromList[2,1]
+    , testCase "tree from list one three elements 1" $
+    Node 3 (Node 2 (Node 1 EmptyTree EmptyTree) EmptyTree) EmptyTree @=? fromList[1,2,3]
+    , testCase "tree from list one three elements 2" $
+    Node 2 (Node 1 EmptyTree EmptyTree) (Node 3 EmptyTree EmptyTree) @=? fromList[1,3,2]
+    , testCase "tree from list one three elements 3" $
+    Node 3 (Node 1 EmptyTree (Node 2 EmptyTree EmptyTree)) EmptyTree @=? fromList[2,1,3]
+    , testCase "tree from list one three elements 4" $
+    Node 1 EmptyTree (Node 3 (Node 2 EmptyTree EmptyTree) EmptyTree) @=? fromList[2,3,1]
+    , testCase "tree from list one three elements 5" $
+    Node 2 (Node 1 EmptyTree EmptyTree) (Node 3 EmptyTree EmptyTree) @=? fromList[3,1,2]
+    , testCase "tree from list one three elements 6" $
+    Node 1 EmptyTree (Node 2 EmptyTree (Node 3 EmptyTree EmptyTree)) @=? fromList[3,2,1]
   ]
