40100eebd623ddc1248c034c53be3c5ea8a1c591
rojoangel <rojo.angel@gmail.com>
2015-09-08 11:58:31 +0200

    refactor: remove intermadiate sieve function

--- 10-sieve-of-eratosthenes/Sieve.hs
+++ 10-sieve-of-eratosthenes/Sieve.hs
@@ -1,8 +1,7 @@
 module Sieve where
 
 primes :: [Int] -> [Int]
-primes = sieve
-  where sieve []      = []
-        sieve (x:xs)
-          | x < 2     = sieve xs
-          | otherwise = x : sieve [x' | x' <- xs, x' `mod` x /= 0]
+primes []     = []
+primes (x:xs)
+  | x < 2     = primes xs
+  | otherwise = x : primes [x' | x' <- xs, x' `mod` x /= 0]
